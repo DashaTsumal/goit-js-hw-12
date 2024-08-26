2e3de7ed9afa0cb6802f2966e2bab3f0544bc003
Dasha_Tsumbal <dasha.tsumbal21@gmail.com>
2024-08-26 14:59:30 +0200

    main

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+export async function fetchParams({ q = '', page = 1, per_page = 15 }) {
+  const API_KEY = '44767976-5c84653ee99974363117d019c';
+  const OPTIONS = new URLSearchParams({
+    key: API_KEY,
+    q,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page,
+    per_page,
+  });
+
+   try {
+    const response = await axios.get(`?${OPTIONS.toString()}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data from Pixabay:', error);
+    throw error;
+  }
+}
